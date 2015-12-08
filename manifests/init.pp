1d1f9e8157257e47f4b3024d03fe8f8496f41cf7
Shaun Rampersad <shaun.rampersad@fnb.co.za>
2015-12-08 18:07:59 +0200

    WIP

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,43 +6,19 @@
 # Parameters
 # ----------
 #
-# Document parameters here.
-#
 # * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
 #
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'logstash':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2015 Your name here, unless otherwise noted.
-#
-class logstash {
+class logstash (
+  $package_name = $::logstash::params::package_name,
+  $service_name = $::logstash::params::service_name,
+) inherits ::logstash::params {
 
+  # validate parameters here
 
+  class { '::logstash::install': } ->
+  class { '::logstash::config': } ~>
+  class { '::logstash::service': } ->
+  Class['::logstash']
 }
