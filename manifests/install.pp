85d8f2e1af8db9232d151c4e22d18c50e507e514
Shaun Rampersad <shaun.rampersad@fnb.co.za>
2015-12-09 15:06:49 +0200

    Defined install class

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,7 +4,35 @@
 #
 class logstash::install {
 
-  package { $::logstash::package_name:
-    ensure => present,
+
+  class { '::staging':
+    path  => '${::logstash::install_dir}/logstash/.staging',
+    mode  => '0700',
+    owner => $::logstash::user,
+    group => $::logstash::group,
   }
+
+  staging::file { "logstash-${::logstash::version}.tar.gz":
+    source => "${::logstash::download_url}/logstash-${::logstash::version}.tar.gz",
+  } ->
+  
+  staging::extract  { "logstash-${::logstash::version}.tar.gz":
+    target => "${::logstash::install_dir}/logstash",
+    creates => "${::logstash::install_dir}/logstash/logstash-${::logstash::version}/NOTICE.TXT",
+    require => Staging::File["logstash-${::logstash::version}.tar.gz",
+  } 
+
+  file { "${::logstash::install_dir}/logstash/logstash-${::logstash::version}/config" :
+    ensure  => directory,
+    owner   => "$::logstash::user",
+    group   => "$::logstash::group",
+    mode    => "0640",
+  }
+
+  file { "${::logstash::install_dir}/logstash/latest" :
+    ensure  => link,
+    target   => "${::logstash::install_dir}/logstash/logstash-${::logstash::version}",
+  }
+
+
 }
